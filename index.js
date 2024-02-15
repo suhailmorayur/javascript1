17e22e06395c0cc09664731bdab14b11bd7367c9
suhailmorayur <147247503+suhailmorayur@users.noreply.github.com>
2024-02-15 20:47:53 +0530

    js upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let mark = 48
+
+if ( mark >= 90) {
+    console.log("A+")
+}
+
+else if ( mark >= 80) {
+    console.log("A")
+}
+
+else if ( mark >= 70) {
+    console.log("B+")
+}
+
+else if ( mark >= 60) {
+    console.log("B")
+}
+
+ else if ( mark >= 50) {
+    console.log("C+")
+}
+ else  {
+    console.log("fail")
+}
+
+
+function showAlert(){
+    alert("Hello World")
+}
